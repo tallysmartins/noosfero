2ef130ea0bc2624b1cc25c4ccb0f56aa85b95b6f
Gabriela Navarro <navarro1703@gmail.com>
2014-08-21 13:00:12 +0000

    Add method to get public institutions from juridical natures
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>

--- lib/juridical_nature.rb
+++ lib/juridical_nature.rb
@@ -5,4 +5,8 @@ class JuridicalNature < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  def public_institutions
+    Institution.where(:type=>"PublicInstitution", :governmental_power_id=>self.id)
+  end
 end
