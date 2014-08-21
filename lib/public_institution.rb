2adffb01a1e28d9fa0928516dc920067f79b30e1
Gabriela Navarro <navarro1703@gmail.com>
2014-08-21 14:32:27 +0000

    instituion_fields: New validations to institution.
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Parley Martins <parley@outlook.com>

--- lib/public_institution.rb
+++ lib/public_institution.rb
@@ -1,5 +1,7 @@
 class PublicInstitution < Institution
-  validates :acronym, :governmental_power, :governmental_sphere, :presence=>true
+  validates :governmental_power, :governmental_sphere, :juridical_nature, :presence=>true
+  validates_uniqueness_of :acronym, :allow_blank => true, :allow_nil => true
   validates_uniqueness_of :cnpj, :unit_code, :allow_nil => true, :allow_blank => true
   validates_format_of :cnpj, :with => /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/, :allow_nil => true, :allow_blank => true
+  
 end
