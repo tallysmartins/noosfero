a8f13a10cb4326e5ca1c82ef4121a91b317ed758
Gabriela Navarro <navarro1703@gmail.com>
2014-08-21 14:32:27 +0000

    instituion_fields: New validations to institution.
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Parley Martins <parley@outlook.com>

--- lib/institution.rb
+++ lib/institution.rb
@@ -15,10 +15,12 @@ class Institution < ActiveRecord::Base
   before_save :verify_institution_type
 
   belongs_to :community
-
+  
   scope :search_institution, lambda{ |value|
     where("name ilike ? OR acronym ilike ?", "%#{value}%", "%#{value}%" )
   }
+
+  validate :validate_country, :validate_state, :validate_city 
 
   protected
 
@@ -29,5 +31,17 @@ class Institution < ActiveRecord::Base
       self.errors.add(:type, _("invalid, only public and private institutions are allowed."))
       false
     end
+  end
+
+  def validate_country
+    self.errors.add(:country, _("can't be blank")) if self.community.country.blank?  && self.errors[:country].blank?
+  end
+
+  def validate_state
+    self.errors.add(:state, _("can't be blank")) if self.community.state.blank?  && self.errors[:state].blank?
+  end
+
+  def validate_city
+    self.errors.add(:city, _("can't be blank")) if self.community.city.blank?  && self.errors[:city].blank?
   end
 end
