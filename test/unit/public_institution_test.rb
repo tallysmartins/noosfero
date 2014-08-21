7c6a23c0619664dad2f470e5880e3100be365a89
Gabriela Navarro <navarro1703@gmail.com>
2014-08-21 13:01:05 +0000

    Add test for new institution fields.
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Parley Martins <parley@outlook.com>

--- test/unit/public_institution_test.rb
+++ test/unit/public_institution_test.rb
@@ -4,22 +4,26 @@ class PublicInstitutionTest < ActiveSupport::TestCase
   def setup
     govPower = GovernmentalPower.create(:name=>"Some Gov Power")
     govSphere = GovernmentalSphere.create(:name=>"Some Gov Sphere")
+    juriNature = JuridicalNature.create(:name => "Some jurid nature")
+    community = Community.create(:name => "Simple Public Institution")
 
     @institution = PublicInstitution::new :name=>"Simple Public Institution", :acronym=>"SPI",
-      :governmental_power=>govPower, :governmental_sphere=>govSphere
+      :governmental_power=>govPower, :governmental_sphere=>govSphere, :juridical_nature => juriNature
+
+    @institution.community = community
+    @institution.community.country = "BR"
+    @institution.community.state = "DF"
+    @institution.community.city = "Gama"
   end
 
   should "save without a cnpj" do
     @institution.cnpj = nil
-
-    assert @institution.save
+    assert @institution.save 
   end
 
-  should "Not save institution without an acronym" do
+  should "save institution without an acronym" do
     @institution.acronym = nil
-
-    assert !@institution.save
-    assert @institution.errors.full_messages.include? "Acronym can't be blank"
+    assert @institution.save
   end
 
   should "Not save institution without a governmental_power" do
@@ -34,5 +38,12 @@ class PublicInstitutionTest < ActiveSupport::TestCase
 
     assert !@institution.save
     assert @institution.errors.full_messages.include? "Governmental sphere can't be blank"
+  end
+
+  should "not save institution without juridical nature" do
+    @institution.juridical_nature = nil
+
+    assert !@institution.save
+    assert @institution.errors.full_messages.include? "Juridical nature can't be blank"
   end
 end
