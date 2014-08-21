a04fdedc7dc028ef4aa763fd404c1b9a73c0ffde
Gabriela Navarro <navarro1703@gmail.com>
2014-08-21 13:01:05 +0000

    Add test for new institution fields.
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Parley Martins <parley@outlook.com>

--- test/unit/institution_test.rb
+++ test/unit/institution_test.rb
@@ -2,15 +2,46 @@ require File.dirname(__FILE__) + '/../../../../test/test_helper'
 
 class InstitutionTest < ActiveSupport::TestCase
 
-  should "save public institutions without name" do
-    institution = Institution::new
-    assert !institution.save
-    assert institution.errors.full_messages.include? "Name can't be blank"
+  def setup
+    community = Community.create(:name => "Simple Public Institution")
+
+    @institution = Institution::new :name=>"Simple Institution"
+
+    @institution.community = community
+    @institution.community.country = "BR"
+    @institution.community.state = "DF"
+    @institution.community.city = "Gama"
+  end
+
+  should "not save institutions without name" do
+    @institution.name = nil
+    assert !@institution.save
+    assert @institution.errors.full_messages.include? "Name can't be blank"
   end
 
   should "not save if institution has invalid type" do
-    institution = Institution::new :name => "teste", :type => "Other type"
-    assert !institution.save
-    assert institution.errors.full_messages.include? "Type invalid, only public and private institutions are allowed."
+    @institution.type = "Other type"
+    assert !@institution.save
+    assert @institution.errors.full_messages.include? "Type invalid, only public and private institutions are allowed."
+  end
+  
+  should "not save without country" do 
+    @institution.community.country = nil
+    assert !@institution.save, "Country can't be blank"
+    assert @institution.errors.full_messages.include? "Country can't be blank"
+  end
+
+  should "not save without state" do 
+    @institution.community.state = nil
+
+    assert !@institution.save, "State can't be blank"
+    assert @institution.errors.full_messages.include? "State can't be blank"
+  end
+
+  should "not save without city" do
+    @institution.community.city = nil
+
+    assert !@institution.save, "City can't be blank"
+    assert @institution.errors.full_messages.include? "City can't be blank"
   end
 end
--- test/unit/private_institution_test.rb
+++ test/unit/private_institution_test.rb
@@ -2,8 +2,13 @@ require File.dirname(__FILE__) + '/../../../../test/test_helper'
 
 class PrivateInstitutionTest < ActiveSupport::TestCase
   def setup
+    community = Community.create(:name => "Simple Private Institution")
     @institution = PrivateInstitution::new :name=>"Simple Private Institution",
       :cnpj=>"00.000.000/0001-00"
+    @institution.community = community
+    @institution.community.country = "BR"
+    @institution.community.state = "DF"
+    @institution.community.city = "Gama"
   end
 
   should "not save without a cnpj" do
@@ -18,8 +23,16 @@ class PrivateInstitutionTest < ActiveSupport::TestCase
 
     sec_institution = PrivateInstitution::new :name=>"Another Private Institution",
       :cnpj=>"00.000.000/0001-00"
+    sec_institution.community = Community.create(:name => "Another Private Institution")
 
     assert !sec_institution.save
     assert sec_institution.errors.full_messages.include? "Cnpj has already been taken"
+  end
+
+  should "save without fantasy name" do
+    @institution.acronym = nil
+    @institution.community.save
+
+    assert @institution.save
   end
 end
