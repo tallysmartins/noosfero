52915c62a511403cd0df2baecad2f08f0c1de804
David Carlos <ddavidcarlos1392@gmail.com>
2014-08-28 12:02:51 -0300

    software_search: Add value to vocabulary helper
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- lib/controlled_vocabulary_helper.rb
+++ lib/controlled_vocabulary_helper.rb
@@ -1,14 +1,15 @@
 module ControlledVocabularyHelper
 
   def self.get_categories_as_options
-    categories = ["<option>Any</option>".html_safe]
+    categories = ["<option value = #{""} >Any</option>".html_safe]
+    value = 1
 
     ControlledVocabulary.attribute_names.each do |attribute|
       if attribute.to_s != "id" && attribute.to_s != "software_info_id" then
-        categories << "<option>#{attribute.titleize}</option>".html_safe
+        categories << "<option value = #{attribute} >#{attribute.titleize}</option>".html_safe
+        value+=1
       end
     end
-
     categories
   end
 end
