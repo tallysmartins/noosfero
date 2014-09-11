44739fca11023e22f076c01ad94e5f53c4479b99
Gabriela Navarro <navarro1703@gmail.com>
2014-09-11 11:42:10 -0300

    corrections: Fix registration bug because javascript behaviour
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: Charles Oliveira <18oliveira.charles@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>

--- public/mpog-user-validations.js
+++ public/mpog-user-validations.js
@@ -30,7 +30,8 @@ function put_brazil_based_on_email(){
     }
   });
 
-  jQuery("#profile_data_country").prop('disabled', gov_suffix);
+  jQuery("#profile_data_country").find(':not(:selected)').css('display', (gov_suffix?'none':'block'));
+
 
   check_reactivate_account(value, input_object)
 }
@@ -62,6 +63,4 @@ jQuery(document).ready(function(){
   jQuery('#secondary_email_field').focus(function() { jQuery('#secondary-email-balloon').fadeIn('slow'); });
   jQuery('#secondary_email_field').blur(function() { jQuery('#secondary-email-balloon').fadeOut('slow'); });
 
-  jQuery('#area_interest_field').focus(function() { jQuery('#area-interest-balloon').fadeIn('slow'); });
-  jQuery('#area_interest_field').blur(function() { jQuery('#area-interest-balloon').fadeOut('slow'); });
 });
