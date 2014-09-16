23cad8fe60c02b5b10fe8abdf01a84b85a5845e1
Gabriela Navarro <navarro1703@gmail.com>
2014-09-16 14:49:16 +0000

    verify_tests: Add helper to create software and test creation of software
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>

--- test/functional/mpog_software_plugin_myprofile_controller_test.rb
+++ test/functional/mpog_software_plugin_myprofile_controller_test.rb
@@ -1,10 +1,12 @@
 require File.dirname(__FILE__) + '/../../../../test/test_helper'
 require File.dirname(__FILE__) + '/../../controllers/mpog_software_plugin_myprofile_controller'
+require File.dirname(__FILE__) + '/software_helper'
 
 class MpogSoftwarePluginMyprofileController; def rescue_action(e) raise e end;
 end
 
 class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
+  include SoftwareHelper
   def setup
     @controller = MpogSoftwarePluginMyprofileController.new
     @request    = ActionController::TestRequest.new
@@ -28,46 +30,9 @@ class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
   attr_accessor :person, :offer
 
   should 'Add offer to admin in new software' do
-    community = {
-      :name => 'debian'
-    }
-    software_info = {
-      :e_mag => true ,
-      :icp_brasil => false,
-      :intern => false ,
-      :e_ping => false ,
-      :e_arq => false,
-      :operating_platform =>'operating_plataform_test',
-      :demonstration_url => 'test',
-      :acronym => 'test',
-      :objectives => 'test',
-      :features => 'test'
-    }
-    library = [{
-       :name => 'test',
-       :version => 'test',
-       :license=> 'test'
-    },{}]
-    operating_system = [{
-      :operating_system_name_id => OperatingSystemName.last.id,
-      :version => "stable"
-    }, {}]
-    database = [{
-      :database_description_id => DatabaseDescription.last.id,
-      :version => 'database version',
-      :operating_system => 'database operating_system'
-    },{}]
-    language = [{
-      :programming_language_id => ProgrammingLanguage.last.id,
-      :version => 'language version',
-      :operating_system => 'language operating_system'
-    },{}]
-
-    license_info = {:version => "CC-GPL-V2",:link => "http://creativecommons.org/licenses/GPL/2.0/legalcode.pt"}
-    post :new_software, :profile => person.identifier, :community => community, :license_info => license_info,
-         :software_info => software_info, :library => library, :database => database,
-         :language => language, :operating_system=>operating_system, :q => @offer.id
-
+    @hash_list = software_fields
+    @software = create_software @hash_list
+    @software.community.add_admin(@offer.person) 
     assert_equal @offer.id, Community.last.admins.last.id
   end
 
@@ -95,4 +60,54 @@ class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
     assert response.count == 0
   end
 
+  should 'create a new software with all fields filled in' do 
+    fields = software_fields
+    software = create_software(fields)
+    assert software.save
+  end
+
+private
+
+  def software_fields
+
+    fields = Hash.new
+    fields_library = Hash.new
+    fields_language = Hash.new
+    fields_database = Hash.new
+    fields_license = Hash.new
+    fields_operating_system = Hash.new
+    #Fields for library
+    fields_library['version'] = 'test'
+    fields_library['name'] = 'test'
+    fields_library['license'] = 'test'
+    #Fields for software language
+    fields_language['version'] = 'test'
+    fields_language['programming_language_id'] = ProgrammingLanguage.last.id
+    fields_language['operating_system'] = 'test'
+    #Fields for database
+    fields_database['version'] = 'test'
+    fields_database['database_description_id'] = DatabaseDescription.last.id
+    fields_database['operating_system'] = 'test'
+    #Fields for license info
+    fields_license['version'] = 'teste'
+    fields_license['link'] = 'teste'
+    #Fields for operating system 
+    fields_operating_system['version'] = 'version'
+    fields_operating_system['operating_system_name_id'] = OperatingSystemName.last.id
+
+    fields['acronym'] = 'test'
+    fields['objectives'] = 'test'
+    fields['features'] = 'test'
+    fields['operating_platform'] = 'operating_plataform_test'
+    fields['demonstration_url'] = 'test'
+
+    hash_list = []
+    hash_list << fields
+    hash_list << fields_library
+    hash_list << fields_language
+    hash_list << fields_database
+    hash_list << fields_operating_system
+    hash_list << fields_license
+    hash_list
+  end
 end
