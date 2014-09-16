429bc4ce7e45900e27e4590184bd6866d1b37033
Antonio Terceiro <terceiro@softwarelivre.org>
2014-09-16 17:21:07 -0300

    use JSON for config file + http_proxy

--- dev-install.sh
+++ dev-install.sh
@@ -7,17 +7,26 @@ host=$(vagrant ssh-config | awk '{ if ($1 == "HostName") { print($2) }}')
 port=$(vagrant ssh-config | awk '{ if ($1 == "Port") { print($2) }}')
 key=$(vagrant ssh-config | awk '{ if ($1 == "IdentityFile") { print($2) }}')
 
-cat > colab/environments.yml <<EOF
-dev:
-  hosts:
-    - $host
-  key_filename: $key
-  port: $port
-  is_vagrant: True
-  superuser: 'vagrant'
+if [ -n "$http_proxy" ]; then
+  proxy="\"command_prefixes\": [\"http_proxy='$http_proxy'\"],"
+else
+  proxy=
+fi
+
+cat > colab/environments.json <<EOF
+{
+  "dev": {
+    $proxy
+    "hosts": ["$host"],
+    "key_filename": "$key",
+    "port": "$port",
+    "is_vagrant": true,
+    "superuser": "vagrant"
+  }
+}
 EOF
 
-sudo apt-get install -qy python-yaml fabric
+dpkg-query --show fabric >/dev/null || sudo apt-get install -qy fabric
 
 (
   cd colab
