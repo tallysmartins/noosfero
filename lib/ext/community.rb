92cac97486903f0ec0b0f1757c86e5ab75648216
Fabio Teixeira <fabio1079@gmail.com>
2014-09-18 16:58:34 -0300

    verify_tests(BUG): Create software after admin approves
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Luiz Matos <luizff.matos@gmail.com>

--- lib/ext/community.rb
+++ lib/ext/community.rb
@@ -9,23 +9,23 @@ class Community
 
   def self.create_after_moderation(requestor, attributes = {}, software_info = nil, license_info = nil, controlled_vocabulary = nil)
     community = Community.new(attributes)
-    if community.environment.enabled?('admin_must_approve_new_communities')
-      CreateCommunity.create(attributes.merge(:requestor => requestor))
-    else
-      community = Community.create(attributes)
 
-      if not software_info.nil?
-        if not license_info.nil?
-          software_info.license_info = license_info
-        end
-
-        if not controlled_vocabulary.nil?
-          software_info.controlled_vocabulary = controlled_vocabulary
-        end
-
-        community.software_info = software_info
+    if not software_info.nil?
+      if not license_info.nil?
+        software_info.license_info = license_info
       end
 
+      if not controlled_vocabulary.nil?
+        software_info.controlled_vocabulary = controlled_vocabulary
+      end
+      software_info.save
+    end
+
+    if community.environment.enabled?('admin_must_approve_new_communities')
+      cc = CreateCommunity.create(attributes.merge(:requestor => requestor, :software_info=>software_info))
+    else
+      community = Community.create(attributes)
+      community.software_info = software_info
       community.add_admin(requestor)
     end
     community
