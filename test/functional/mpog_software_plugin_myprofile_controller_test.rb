e80430110ac3d8d72118e856b3578f59c7a6e9f5
Gabriela Navarro <navarro1703@gmail.com>
2014-09-18 11:59:27 -0300

    Add tests and helper for create new software.
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>
    Signed-off-by: Parley Martins <parley@outlook.com>

--- test/functional/mpog_software_plugin_myprofile_controller_test.rb
+++ test/functional/mpog_software_plugin_myprofile_controller_test.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/../../../../test/test_helper'
 require File.dirname(__FILE__) + '/../../controllers/mpog_software_plugin_myprofile_controller'
-require File.dirname(__FILE__) + '/software_helper'
+require File.dirname(__FILE__) + '/software_test_helper'
 
 class MpogSoftwarePluginMyprofileController; def rescue_action(e) raise e end;
 end
 
 class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
-  include SoftwareHelper
+  include SoftwareTestHelper
   def setup
     @controller = MpogSoftwarePluginMyprofileController.new
     @request    = ActionController::TestRequest.new
@@ -22,9 +22,9 @@ class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
     OperatingSystemName.create(:name=>"Debian")
 
     login_as(@person.user.login)
-    e = Environment.default
-    e.enable_plugin('MpogSoftwarePlugin')
-    e.save!
+    @e = Environment.default
+    @e.enable_plugin('MpogSoftwarePlugin')
+    @e.save!
   end
 
   attr_accessor :person, :offer
@@ -32,8 +32,9 @@ class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
   should 'Add offer to admin in new software' do
     @hash_list = software_fields
     @software = create_software @hash_list
-    @software.community.add_admin(@offer.person) 
-    assert_equal @offer.id, Community.last.admins.last.id
+    @software.community.add_admin(@offer.person)
+    @software.save
+    assert_equal @offer.id, @software.community.admins.last.id
   end
 
   should 'search new offers while creating a new software' do
@@ -62,52 +63,12 @@ class MpogSoftwarePluginMyprofileControllerTest < ActionController::TestCase
 
   should 'create a new software with all fields filled in' do 
     fields = software_fields
-    software = create_software(fields)
-    assert software.save
+
+    post :new_software, :profile => person.identifier, :community => fields[6], :license_info => fields[5],
+                        :software_info => fields[0], :library => fields[1], :database => fields[3],
+                        :language => fields[2], :operating_system=> fields[4]
+    assert @response.success?
   end
 
-private
 
-  def software_fields
-
-    fields = Hash.new
-    fields_library = Hash.new
-    fields_language = Hash.new
-    fields_database = Hash.new
-    fields_license = Hash.new
-    fields_operating_system = Hash.new
-    #Fields for library
-    fields_library['version'] = 'test'
-    fields_library['name'] = 'test'
-    fields_library['license'] = 'test'
-    #Fields for software language
-    fields_language['version'] = 'test'
-    fields_language['programming_language_id'] = ProgrammingLanguage.last.id
-    fields_language['operating_system'] = 'test'
-    #Fields for database
-    fields_database['version'] = 'test'
-    fields_database['database_description_id'] = DatabaseDescription.last.id
-    fields_database['operating_system'] = 'test'
-    #Fields for license info
-    fields_license['version'] = 'teste'
-    fields_license['link'] = 'teste'
-    #Fields for operating system 
-    fields_operating_system['version'] = 'version'
-    fields_operating_system['operating_system_name_id'] = OperatingSystemName.last.id
-
-    fields['acronym'] = 'test'
-    fields['objectives'] = 'test'
-    fields['features'] = 'test'
-    fields['operating_platform'] = 'operating_plataform_test'
-    fields['demonstration_url'] = 'test'
-
-    hash_list = []
-    hash_list << fields
-    hash_list << fields_library
-    hash_list << fields_language
-    hash_list << fields_database
-    hash_list << fields_operating_system
-    hash_list << fields_license
-    hash_list
-  end
 end
