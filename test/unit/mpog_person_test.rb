30f5ba9a1210b3363067309f9374a6ba65e0861e
Fabio Teixeira <fabio1079@gmail.com>
2014-09-26 18:47:08 -0300

    correcoes_aderencia: Unit test for person full name validation
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

---
+++ test/unit/mpog_person_test.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../../../../test/test_helper'
+
+class MpogSoftwarePluginPersonTest < ActiveSupport::TestCase
+  should 'save person with a valid full name' do
+    p = Person::new :name=>"S1mpl3 N4m3", :identifier=>"simple-name"
+    p.user = fast_create(:user)
+
+    assert_equal true, p.save
+  end
+
+  should 'not save person whose name has not capital letter' do
+    p = Person::new :name=>"simple name"
+    assert !p.save, _("Name Should begin with a capital letter and no special characters")
+  end
+
+  should 'not save person whose name has special characters' do
+    p = Person::new :name=>"Simple N@me"
+
+    assert !p.save , _("Name Should begin with a capital letter and no special characters")
+  end
+end
