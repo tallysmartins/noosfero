f5240f14262e48c9ee5915052e6f6d06a9600d4b
Gust <darksshades@hotmail.com>
2014-10-09 11:56:20 -0300

    Add dropdown section, fix javascript
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>
    Signed-off-by: Parley Martins <parley@outlook.com>

--- public/mpog-software.js
+++ public/mpog-software.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function(){
-  jQuery(".control-panel")[0].innerHTML = jQuery(".control-panel-edit-profile-group:contains('Software')")[0].outerHTML + jQuery(".control-panel")[0].innerHTML
-  jQuery(".control-panel-edit-profile-group:contains('Software')")[1].remove()
+  if(jQuery(".control-panel").size() > 0 && jQuery(".control-panel-edit-profile-group:contains('Software')").size() > 0 ) {
+    jQuery(".control-panel")[0].innerHTML = jQuery(".control-panel-edit-profile-group:contains('Software')")[0].outerHTML + jQuery(".control-panel")[0].innerHTML
+    jQuery(".control-panel-edit-profile-group:contains('Software')")[1].remove()
+  }
 });
