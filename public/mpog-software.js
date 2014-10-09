323d07c4d83d6813a94b1435ff9fd54cc2c80fa7
Gust <darksshades@hotmail.com>
2014-10-09 10:36:01 -0300

    Add javascript to place Software button first
    Signed-off-by: Luciano Prestes <lucianopcbr@gmail.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- public/mpog-software.js
+++ public/mpog-software.js
@@ -1,4 +1,4 @@
-
 jQuery(document).ready(function(){
-};
-//jQuery(".control-panel")[0].innerHTML = jQuery(".control-panel-edit-profile-group")[1].outerHTML + jQuery(".control-panel")[0].innerHTML
+  jQuery(".control-panel")[0].innerHTML = jQuery(".control-panel-edit-profile-group:contains('Software')")[0].outerHTML + jQuery(".control-panel")[0].innerHTML
+  jQuery(".control-panel-edit-profile-group:contains('Software')")[1].remove()
+});
