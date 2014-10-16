aa66130d0ade3adfcf754c65aa7461cf5207d892
ParleyMartins <parleypachecomartins@gmail.com>
2014-10-16 12:01:18 -0300

    New profile page to software
    Since Software is not a profile, a few adaptations were needed to print
    its informations.

--- lib/ext/profile_helper.rb
+++ lib/ext/profile_helper.rb
@@ -1,3 +1,19 @@
 module ProfileHelper extend ProfileHelper
   PERSON_CATEGORIES[:mpog_profile_information] = [:secondary_email, :institutions]
+
+  def display_mpog_field(title, profile, field, force = false)
+    unless force || profile.may_display_field_to?(field, user)
+      return ''
+    end
+    value = profile.send(field)
+    if !value.blank?
+      if block_given?
+        value = yield(value)
+      end
+      content_tag('tr', content_tag('td', title, :class => 'field-name') + content_tag('td', value))
+    else
+      ''
+    end
+  end
+
 end
