58889ba773384901f004284174f90c0b7985ab63
Edson Alves <prof.edson.alves.costa@gmail.com>
2014-10-20 17:33:47 +0000

    New repositories tree.

--- build_setuptools_rpms.sh
+++ build_setuptools_rpms.sh
@@ -4,22 +4,24 @@
 mr checkout
 
 # Copiando os arquivos de configuração editados
-CONFIGS="bottle mailman-api"
+CONFIGS="mailman-api"
 
 for cfg in $CONFIGS; do
     echo "Copying $cfg setup.cfg...";
     cp cfg/${cfg}_setup.cfg $cfg/setup.cfg
 done
 
-# Criando os rpms
-DIRS="six simplejson paste bottle mailman-api"
+# Criando os rpms na pasta packages
+DIRS="mailman-api/paste/six mailman-api/paste mailman-api/bottle 
+	mailman-api/simplejson mailman-api"
 
 mkdir -p packages
 
 for dir in $DIRS; do
+    curdir=`pwd`
     cd $dir;
     python setup.py bdist_rpm;
-    cp dist/*.rpm ../packages;
-    cd ..;
+    cp dist/*.rpm $curdir/packages;
+    cd $curdir;
 done
 
