66411419bbf743a6ef2a599f3c97ef9603de825f
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2014-10-29 16:39:45 -0200

    Register software redirect to edit community of software
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Luciano Prestes <lucianopcbr@gmail.com>

--- controllers/mpog_software_plugin_myprofile_controller.rb
+++ controllers/mpog_software_plugin_myprofile_controller.rb
@@ -24,7 +24,6 @@ class MpogSoftwarePluginMyprofileController < MyProfileController
    valid_license = (request.post? && @license_info.valid?)
    if valid_software_info && valid_license && valid_community
     @community = Community.create_after_moderation(user, {:environment => environment}.merge(params[:community]), @software_info, @license_info )
-    redirect_to :controller => 'mpog_software_plugin_myprofile', :action => 'edit_software', :profile => @community.identifier
 
      unless params[:q].nil?
        admins = params[:q].split(/,/).map{|n| environment.people.find n.to_i}
@@ -35,6 +34,7 @@ class MpogSoftwarePluginMyprofileController < MyProfileController
        end
      end
 
+     redirect_to :controller => 'profile_editor', :action => 'edit', :profile => @community.identifier
    else
    #  @list_libraries.each do |lib|
    #    @errors |= lib.errors.full_messages
