37d8ade76cb34951b74c4adb9e92fafba7c0cb0c
Fabio Teixeira <fabio1079@gmail.com>
2014-11-05 11:32:15 -0200

    fix_institution: Fix functional tests
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>

--- test/functional/profile_editor_controller_test.rb
+++ test/functional/profile_editor_controller_test.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/../../../../test/test_helper'
+require File.dirname(__FILE__) + '/institution_test_helper'
 require File.dirname(__FILE__) + '/../../../../app/controllers/my_profile/profile_editor_controller'
 
 class ProfileEditorController; def rescue_action(e) raise e end; end
@@ -19,14 +20,17 @@ class ProfileEditorControllerTest < ActionController::TestCase
     @environment.add_admin(admin)
     @environment.save
 
-    @govPower = GovernmentalPower.create(:name => "Some Gov Power")
-    @govSphere = GovernmentalSphere.create(:name => "Some Gov Sphere")
+    @govPower = GovernmentalPower.create(:name=>"Some Gov Power")
+    @govSphere = GovernmentalSphere.create(:name=>"Some Gov Sphere")
     @juridical_nature = JuridicalNature.create(:name => "Autarquia")
 
+    @institution_list = []
+    @institution_list << InstitutionTestHelper.create_public_institution("Ministerio Publico da Uniao", "MPU", "BR", "DF", "Gama", @juridical_nature, @govPower, @govSphere, "12.345.678/9012-45")
+    @institution_list << InstitutionTestHelper.create_public_institution("Tribunal Regional da Uniao", "TRU", "BR", "DF", "Brasilia", @juridical_nature, @govPower, @govSphere, "12.345.678/9012-90")
   end
 
   should "update institution date_modification when edit profile" do
-    create_public_institution("Ministerio Publico da Uniao", "MPU", "BR", "DF", "Gama", @juridical_nature, @govPower, @govSphere)
+    InstitutionTestHelper.create_public_institution("Just a test name", "JTN", "BR", "DF", "Gama", @juridical_nature, @govPower, @govSphere, "12.345.678/9055-33")
 
     post :edit, :profile => Institution.last.community.identifier, :profile_data => {:name => "Ministerio da Saude"}, :institution => Institution.last
     
@@ -35,9 +39,6 @@ class ProfileEditorControllerTest < ActionController::TestCase
   end
 
   should "add new institution for user into edit profile" do
-    institutions = []
-    institutions << create_public_institution("Ministerio Publico da Uniao", "MPU", "BR", "DF", "Gama", @juridical_nature, @govPower, @govSphere)
-    institutions << create_public_institution("Nova Instituicao", "NIN", "BR", "GO", "Goiania", @juridical_nature, @govPower, @govSphere)
     user = fast_create(User)
     user.person = fast_create(Person)
     user.person.user = user
@@ -46,21 +47,23 @@ class ProfileEditorControllerTest < ActionController::TestCase
 
     params_user = Hash.new
     params_user[:institution_ids] = []
-    institutions.each do |institution|
+
+    @institution_list.each do |institution|
       params_user[:institution_ids] << institution.id
     end
 
     post :edit, :profile => User.last.person.identifier, :user => params_user
 
-    assert_equal institutions.count, User.last.institutions.count
+    assert_equal @institution_list.count, User.last.institutions.count
   end
 
   should "remove institutions for user into edit profile" do
     user = fast_create(User)
     user.person = fast_create(Person)
 
-    user.institutions << create_public_institution("Ministerio Publico da Uniao", "MPU", "BR", "DF", "Gama", @juridical_nature, @govPower, @govSphere)
-    user.institutions << create_public_institution("Nova Instituicao", "NIN", "BR", "GO", "Goiania", @juridical_nature, @govPower, @govSphere)
+    @institution_list.each do |institution|
+      user.institutions << institution
+    end
 
     user.person.user = user
     user.save!
@@ -69,30 +72,10 @@ class ProfileEditorControllerTest < ActionController::TestCase
     params_user = Hash.new
     params_user[:institution_ids] = []
 
+    assert_equal @institution_list.count, User.last.institutions.count
+
     post :edit, :profile => User.last.person.identifier, :user => params_user
 
     assert_equal 0, User.last.institutions.count
   end
-
-  private
-
-  def create_public_institution name, acronym, country, state, city, juridical_nature, gov_p, gov_s
-    institution_community = fast_create(Community)
-    institution_community.name = name
-    institution_community.country = country
-    institution_community.state = state
-    institution_community.city = city
-    institution_community.save!
-
-    institution = PublicInstitution.new
-    institution.community = institution_community
-    institution.name = name
-    institution.juridical_nature = juridical_nature
-    institution.acronym = acronym
-    institution.governmental_power = gov_p
-    institution.governmental_sphere = gov_s
-    institution.save!
-    institution
-  end
-
 end
