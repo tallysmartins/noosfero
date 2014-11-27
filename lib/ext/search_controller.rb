5a1ee775ad6b2a782b5af2713331c7919e3da338
Arthur Del Esposte <arthurmde@gmail.com>
2014-11-27 09:48:29 -0200

    Fix community filtered search

--- lib/ext/search_controller.rb
+++ lib/ext/search_controller.rb
@@ -3,17 +3,22 @@ require_dependency 'search_controller'
 class SearchController
 
   def communities
-      unfiltered_list = visible_profiles(Community).select{ |com| com.name.downcase =~ /#{params[:query].downcase}/}
-      list_without_software_and_institution = []
-      unfiltered_list.each do |p|
-        if p.class == Community and !p.software? and !p.institution?
-          list_without_software_and_institution << p
-        end
+    unfiltered_list = visible_profiles(Community)
+    unless params[:query].nil?
+      unfiltered_list = unfiltered_list.select do |com|
+        com.name.downcase =~ /#{params[:query].downcase}/
       end
-      results = list_without_software_and_institution
-      results = results.paginate(:per_page => 24, :page => params[:page])
-      @searches[@asset] = {:results => results}
-      @search = results
-  end
+    end
 
+    list_without_software_and_institution = []
+    unfiltered_list.each do |p|
+      if p.class == Community and !p.software? and !p.institution?
+        list_without_software_and_institution << p
+      end
+    end
+    results = list_without_software_and_institution
+    results = results.paginate(:per_page => 24, :page => params[:page])
+    @searches[@asset] = {:results => results}
+    @search = results
+  end
 end
