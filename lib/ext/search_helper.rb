dbedeb9fad21e814333ced5aee48291c7f82ae70
Fabio Teixeira <fabio1079@gmail.com>
2014-12-03 17:10:25 -0200

    software-catalog: Fix bug, uninitialized constant SearchHelper::COMMON_PROFILE_LIST_BLOCK
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- lib/ext/search_helper.rb
+++ lib/ext/search_helper.rb
@@ -2,6 +2,7 @@ require_dependency 'search_helper'
 
 module SearchHelper
 
-	COMMON_PROFILE_LIST_BLOCK << :software_infos
+  COMMON_PROFILE_LIST_BLOCK ||= []
+  COMMON_PROFILE_LIST_BLOCK << :software_infos
 
 end
