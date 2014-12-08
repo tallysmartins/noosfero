64ff46bfabed9e3b8c29ed1be216e2436924b267
Gabriela Navarro <navarro1703@gmail.com>
2014-12-08 19:52:12 +0000

    Translations for the plugin
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Luciano Prestes <lucianopcbr@gmail.com>

--- lib/ext/category.rb
+++ lib/ext/category.rb
@@ -2,9 +2,25 @@ require_dependency 'category'
 
 class Category
 
+	SOFTWARE_CATEGORIES = [
+		_("Agriculture, Fisheries and Extraction"),
+		_("Science, Information and Communication"),
+		_("Economy and Finances"),
+		_("Public Administration"),
+		_("Habitation, Sanitation and Urbanism"),
+		_("Individual, Family and Society"),
+		_("Health"),
+		_("Social Welfare and Development"),
+		_("Defense and Security"),
+		_("Education"),
+		_("Government and Politics"),
+		_("Justice and Legislation"),
+		_("International Relationships")
+	]
+
 	def software_infos
 		software_list = self.communities
-		software_list.collect{|x| software_list.delete(x) unless x.software? } 
+		software_list.collect{|x| software_list.delete(x) unless x.software? }
 		software_list
 	end
 
