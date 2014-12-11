886a0f480051c898c6d3fa0d559dc25e015c573c
Fabio Teixeira <fabio1079@gmail.com>
2014-12-11 14:57:18 -0200

    Refactory library.rb
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- lib/library.rb
+++ lib/library.rb
@@ -1,24 +1,10 @@
 class Library < ActiveRecord::Base
-
-  #TODO missing relations?
-
   attr_accessible :name, :version, :license, :software_info_id
 
-  validate :validate_name, :validate_version, :validate_license
-  validates :name, length: { within: 0..20 }
-  validates_length_of :version, maximum: 20, too_long: _("Library is too long (maximum is 20 characters)")
-  validates_length_of :license, maximum: 20, too_long: _("Library is too long (maximum is 20 characters)")
-
-  def validate_name
-    self.errors.add(:name, _("can't be blank")) if self.name.blank? && self.errors[:name].blank?
-  end
-
-  def validate_version
-    self.errors.add(:version, _("can't be blank")) if self.version.blank? && self.errors[:version].blank?
-  end
-
-  def validate_license
-    self.errors.add(:license, _("can't be blank")) if self.license.blank? && self.errors[:license].blank?
-  end
-
+  validates :name, :version, :license,
+            presence: { message: _("can't be blank") },
+            length: {
+              maximum: 20,
+              too_long: _("Too long (maximum is 20 characters)")
+            }
 end
