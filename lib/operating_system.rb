f78b8a0587c59d7ee8886abf54e8d3317f87b1c1
Fabio Teixeira <fabio1079@gmail.com>
2014-12-11 16:47:31 -0200

    Refactory OperatingSystem
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- lib/operating_system.rb
+++ lib/operating_system.rb
@@ -4,7 +4,11 @@ class OperatingSystem < ActiveRecord::Base
   belongs_to :software_info
   belongs_to :operating_system_name
 
-  validates_length_of :version, maximum: 20, too_long: _("Operating system is too long (maximum is 20 characters)")
-
-  validates :version, :operating_system_name, :presence=>true
+  validates :operating_system_name, presence: true
+  validates :version,
+            presence: true,
+            length: {
+              maximum: 20,
+              too_long: _('too long (maximum is 20 characters)')
+            }
 end
--- test/unit/operating_system_validation_test.rb
+++ test/unit/operating_system_validation_test.rb
@@ -14,22 +14,21 @@ class OperatingSystemValidationTest < ActiveSupport::TestCase
   end
 
   should "save OperatingSystem if all fields are filled" do
-   assert @operating_system.save
+   assert_equal true, @operating_system.save
   end
 
   should "not save if OperatingSystem does  not have version" do
    @operating_system.version = " "
-   assert !@operating_system.save
+   assert_equal false, @operating_system.save
   end
 
   should "not save if OperatingSystem does  not have operating_system_name" do
    @operating_system.operating_system_name = nil
-   assert !@operating_system.save
+   assert_equal false, @operating_system.save
   end
 
   should "not save if OperatingSystem have a version too long" do
-    @operating_system.version = "A too long version to be a valid operating system version"
-    assert !@operating_system.save
+    @operating_system.version = "A too long version to be a valid"
+    assert_equal false, @operating_system.save
   end
-
 end
