935274673aa137bd0443263b88d2a6fb9df22b54
Antonio Terceiro <terceiro@softwarelivre.org>
2015-02-09 17:00:15 -0200

    configure PostgreSQL

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -1,9 +1,22 @@
 package 'postgresql-server'
 
-execute 'postgresql-setup initdb || true' do
-  notifies :start, 'service[postgresql]'
+execute 'postgresql-setup initdb || true'
+
+template '/var/lib/pgsql/data/pg_hba.conf' do
+  user 'postgres'
+  group 'postgres'
+  mode 0600
+  notifies :restart, 'service[postgresql]'
+end
+
+template '/var/lib/pgsql/data/postgresql.conf' do
+  user 'postgres'
+  group 'postgres'
+  mode 0600
+  notifies :restart, 'service[postgresql]'
 end
 
 service 'postgresql' do
   action :start
+  supports :restart => true
 end
