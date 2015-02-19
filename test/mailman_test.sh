bafd30eb085c797616737b5f41940bf62b87a261
Antonio Terceiro <terceiro@softwarelivre.org>
2015-02-19 17:21:29 -0200

    mailman_test: extract helper programs
    They are going to be used when testing the mail relay

--- test/mailman_test.sh
+++ test/mailman_test.sh
@@ -6,23 +6,16 @@ test_mailman_running() {
 
 test_mailman_delivery() {
   # create list
-  run_on integration sudo -u mailman /usr/lib/mailman/bin/newlist --quiet foobar foobar@example.com foobar
-  # subscribe us
-  echo 'foobar@example.com' | run_on integration sudo -u mailman /usr/lib/mailman/bin/add_members -r - --welcome-msg=n foobar > /dev/null
+  run_on integration create-list mylist user@example.com
 
   # send message
-  date | run_on integration mail -r foobar@example.com -s test foobar@listas.softwarepublico.dev
+  run_on integration send-email user@example.com mylist@listas.softwarepublico.dev
 
   # wait for message to arrive at the list mailbox
-  mbox=/var/lib/mailman/archives/private/foobar.mbox/foobar.mbox
-  if wait_for integration $mbox; then
-    messages=$(run_on integration sudo grep -i -c ^Message-ID: $mbox)
-  else
-    messages=0
-  fi
+  messages=$(run_on integration wait-for-messages mylist)
 
   # remove list
-  run_on integration sudo -u mailman /usr/lib/mailman/bin/rmlist -a foobar > /dev/null
+  run_on integration remove-list mylist
 
   assertEquals 'Message arrives at mailbox' "1" "$messages"
 }
--- test/test_helper.sh
+++ test/test_helper.sh
@@ -3,22 +3,7 @@ export PATH="$(dirname $0)/bin:$PATH"
 run_on() {
   local vm="$1"
   shift
-  ssh -F .ssh_config "$vm" -- "$@"
-}
-
-# waits until a file exists
-wait_for() {
-  local machine="$1"
-  local file="$2"
-  local total=0
-  while [ "$total" -lt 10 ]; do
-    if run_on "$machine" sudo test -f "$file"; then
-      return 0
-    fi
-    sleep 1
-    total=$(($total + 1))
-  done
-  return 1
+  ssh -F .ssh_config "$vm" -- 'export PATH=/vagrant/test/bin:$PATH;' "$@"
 }
 
 . $(dirname $0)/ip_helper.sh
