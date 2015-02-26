69305a7a035891b91170410b1b39f33d0c103981
Antonio Terceiro <terceiro@softwarelivre.org>
2015-02-26 10:29:40 -0300

    run_on: send commands in a pipe
    Otherwise ssh will call bash -c "COMMAND", which is matched when you do
    things like `run_on $machine pgrep -fa PROCESS` (ssh runs bash -c 'pgrep
    -fa PROCESS').

--- test/test_helper.sh
+++ test/test_helper.sh
@@ -4,7 +4,7 @@ export ROOTDIR="$(readlink -f $(dirname $0)/..)"
 run_on() {
   local vm="$1"
   shift
-  ssh -F .ssh_config "$vm" -- 'export PATH=/vagrant/test/bin:$PATH;' "$@"
+  echo 'export PATH=/vagrant/test/bin:$PATH;' "$@" | ssh -F .ssh_config "$vm"
 }
 
 . $(dirname $0)/ip_helper.sh
