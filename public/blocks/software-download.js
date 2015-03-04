57ccda1eb997e16e5f1fd011f18c42d778060133
Fabio Teixeira <fabio1079@gmail.com>
2015-03-04 17:56:03 -0300

    Refactoty SoftwareDownload block javascript

--- public/blocks/software-download.js
+++ public/blocks/software-download.js
@@ -1,30 +1,35 @@
 modulejs.define('SoftwareDownload', ['jquery'], function($) {
   'use strict';
 
-  function add_new_link(){
-    var newDownload = $(window.download_list_template);
-    newDownload.attr('data-counter-id', 1);
-    $("#droppable-list-downloads").append(newDownload);
-  }
+  var $download_html_template;
 
+  function getDownloadListTemplate() {
+    var block_template = sessionStorage.getItem('download_list_block_template');
 
-  function delete_link(add_element){
-    var deleteDownload = $(add_element).parent().parent().parent();
-    deleteDownload.remove();
-  }
-
-
-  function get_download_list_template() {
-    var blockTemplate = sessionStorage.getItem('download_list_block_template');
-
-    if(blockTemplate && blockTemplate.length > 0) {
-      window.download_list_template = blockTemplate;
+    if(block_template && block_template.length > 0) {
+      $download_html_template = block_template;
     } else {
       $.get('/plugin/software_communities/get_block_template', function(response) {
-        window.download_list_template = response;
+        $download_html_template = response;
         sessionStorage.setItem('download_list_block_template', response);
       });
     }
+  }
+
+
+  function SoftwareDownload() {
+    getDownloadListTemplate();
+  }
+
+
+  SoftwareDownload.prototype.addNewDonwload = function() {
+    var new_download = $($download_html_template);
+    $("#droppable-list-downloads").append(new_download);
+  }
+
+
+  SoftwareDownload.prototype.deleteDownload = function(element) {
+    var delete_download = $(element).parent().parent().parent().remove();
   }
 
 
@@ -35,12 +40,7 @@ modulejs.define('SoftwareDownload', ['jquery'], function($) {
 
 
     init: function() {
-      window.add_new_link = add_new_link;
-      window.delete_link = delete_link;
-
-      if( window.download_list_template === undefined ) {
-        get_download_list_template();
-      }
+      window.softwareDownload = new SoftwareDownload();
     }
   }
 });
