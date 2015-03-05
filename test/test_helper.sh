426d2d1c096f0a545140e0f8db074b345da87f71
Sergio Oliveira <sergio@tracy.com.br>
2015-03-05 14:42:53 -0300

    Fixed shunit2 path

--- test/test_helper.sh
+++ test/test_helper.sh
@@ -8,12 +8,10 @@ run_on() {
 }
 
 load_shunit2() {
-  if [ `which shunit2 > /dev/null 2>&1` ]; then
-    . shunit2
-  elif [ -e /usr/share/shunit2/shunit2 ]; then
+  if [ -e /usr/share/shunit2/shunit2 ]; then
     . /usr/share/shunit2/shunit2
   else
-    echo "Could not find shunit2, please, make sure you have it installed."
+    . shunit2
   fi
 }
 
