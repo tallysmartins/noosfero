58e8d6c27ebd62689c0856a4a10c6beb4114abd9
Fabio Teixeira <fabio1079@gmail.com>
2015-03-05 22:15:25 -0300

    Place software wall js code into its correct module

--- public/views/edit-software.js
+++ public/views/edit-software.js
@@ -98,61 +98,7 @@ modulejs.define('EditSoftware', ['jquery', 'NoosferoRoot', 'AutoComplete', 'NewS
       });
 
 
-      $(".language-button-hide").click(function(event){
-        event.preventDefault();
-        $(".language-info").hide();
-        $(".language-button-show").show();
-        $(".language-button-hide").hide();
-      });
 
-      $(".language-button-show").click(function(event){
-        event.preventDefault();
-        $(".language-info").show();
-        $(".language-button-show").hide();
-        $(".language-button-hide").show();
-      });
-
-      $(".operating-system-button-hide").click(function(event){
-        event.preventDefault();
-        $(".operating-system-info").hide();
-        $(".operating-system-button-show").show();
-        $(".operating-system-button-hide").hide();
-      });
-
-      $(".operating-system-button-show").click(function(event){
-        event.preventDefault();
-        $(".operating-system-info").show();
-        $(".operating-system-button-show").hide();
-        $(".operating-system-button-hide").show();
-      });
-
-      $(".database-button-hide").click(function(event){
-        event.preventDefault();
-        $(".database-info").hide();
-        $(".database-button-show").show();
-        $(".database-button-hide").hide();
-      });
-
-      $(".database-button-show").click(function(event){
-        event.preventDefault();
-        $(".database-info").show();
-        $(".database-button-show").hide();
-        $(".database-button-hide").show();
-      });
-
-      $(".libraries-button-hide").click(function(event){
-        event.preventDefault();
-        $(".libraries-info").hide();
-        $(".libraries-button-show").show();
-        $(".libraries-button-hide").hide();
-      });
-
-      $(".libraries-button-show").click(function(event){
-        event.preventDefault();
-        $(".libraries-info").show();
-        $(".libraries-button-show").hide();
-        $(".libraries-button-hide").show();
-      });
 
       hide_show_public_software_fields();
       $("#software_public_software").click(hide_show_public_software_fields);
--- public/views/profile-tabs-software.js
+++ public/views/profile-tabs-software.js
@@ -1,4 +1,4 @@
-modulejs.define("ProfileTabsSoftware", ["jquery", "EditSoftware"], function($, EditSoftware) {
+modulejs.define("ProfileTabsSoftware", ["jquery"], function($) {
   "use strict";
 
   function hide_infos(){
@@ -13,6 +13,65 @@ modulejs.define("ProfileTabsSoftware", ["jquery", "EditSoftware"], function($, E
   }
 
 
+  function set_show_hide_dynamic_table_events() {
+    $(".language-button-hide").click(function(event){
+      event.preventDefault();
+      $(".language-info").hide();
+      $(".language-button-show").show();
+      $(".language-button-hide").hide();
+    });
+
+    $(".language-button-show").click(function(event){
+      event.preventDefault();
+      $(".language-info").show();
+      $(".language-button-show").hide();
+      $(".language-button-hide").show();
+    });
+
+    $(".operating-system-button-hide").click(function(event){
+      event.preventDefault();
+      $(".operating-system-info").hide();
+      $(".operating-system-button-show").show();
+      $(".operating-system-button-hide").hide();
+    });
+
+    $(".operating-system-button-show").click(function(event){
+      event.preventDefault();
+      $(".operating-system-info").show();
+      $(".operating-system-button-show").hide();
+      $(".operating-system-button-hide").show();
+    });
+
+    $(".database-button-hide").click(function(event){
+      event.preventDefault();
+      $(".database-info").hide();
+      $(".database-button-show").show();
+      $(".database-button-hide").hide();
+    });
+
+    $(".database-button-show").click(function(event){
+      event.preventDefault();
+      $(".database-info").show();
+      $(".database-button-show").hide();
+      $(".database-button-hide").show();
+    });
+
+    $(".libraries-button-hide").click(function(event){
+      event.preventDefault();
+      $(".libraries-info").hide();
+      $(".libraries-button-show").show();
+      $(".libraries-button-hide").hide();
+    });
+
+    $(".libraries-button-show").click(function(event){
+      event.preventDefault();
+      $(".libraries-info").show();
+      $(".libraries-button-show").hide();
+      $(".libraries-button-hide").show();
+    });
+  }
+
+
   return {
     isCurrentPage: function() {
       return $("#software-fields").length === 1;
@@ -21,8 +80,7 @@ modulejs.define("ProfileTabsSoftware", ["jquery", "EditSoftware"], function($, E
 
     init: function() {
       hide_infos();
-
-      EditSoftware.init();
+      set_show_hide_dynamic_table_events();
     }
   }
 });
