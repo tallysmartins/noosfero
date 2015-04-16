c024c36579c37d6412187cdff9fbcf6bd2e02b4a
Sergio Oliveira <sergio@tracy.com.br>
2015-04-16 18:04:03 -0300

    Config ip_forward

--- cookbooks/reverse_proxy/recipes/default.rb
+++ cookbooks/reverse_proxy/recipes/default.rb
@@ -7,6 +7,14 @@ cookbook_file "/etc/nginx/#{node['config']['external_hostname']}.crt" do
   notifies :restart, 'service[nginx]'
 end
 
+cookbook_file "/etc/sysctl.d/ip_forward.conf" do
+  owner 'root'
+  group 'root'
+  mode 0644
+end
+
+execute 'sysctl -w net.ipv4.ip_forward=1'
+
 cookbook_file "/etc/nginx/#{node['config']['external_hostname']}.key" do
   owner 'root'
   group 'root'
