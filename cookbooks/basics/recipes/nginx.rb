77730dfede7fb2c19c1fc9545059a44712272d72
Macartur Sousa <macartur.sc@gmail.com>
2015-04-16 16:00:04 -0300

    Added nginx permission to write files  *.error.log
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- cookbooks/basics/recipes/nginx.rb
+++ cookbooks/basics/recipes/nginx.rb
@@ -4,3 +4,15 @@ service 'nginx' do
   action :enable
   supports :restart => true
 end
+
+################################
+#  SELinux: allow nginx to use log files
+################################
+cookbook_file '/etc/selinux/local/nginx.te' do
+  notifies :run, 'execute[selinux-nginx]'
+end
+
+execute 'selinux-nginx' do
+  command 'selinux-install-module /etc/selinux/local/nginx.te'
+  action  :nothing
+end
