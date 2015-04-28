9f4b078b2033f934611bd19d374f58088c4df1ef
Athos Ribeiro <athoscribeiro@gmail.com>
2015-04-28 09:17:28 -0300

    disable firewalld if installed
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- cookbooks/basics/recipes/default.rb
+++ cookbooks/basics/recipes/default.rb
@@ -36,6 +36,15 @@ service 'ntpd' do
   action [:enable, :start]
 end
 
+service 'firewalld' do
+  action [:disable, :stop]
+  ignore_failure true
+end
+
+service 'sshd' do
+  action [:enable]
+end
+
 # FIXME on Debian it's postgresql-client
 package 'postgresql'
 
