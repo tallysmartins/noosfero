d952d30b7fcce15219cfcf9a2a0791da1ac11e62
Antonio Terceiro <terceiro@softwarelivre.org>
2015-05-29 08:29:26 -0300

    Documentando gestão de par de chaves SSL

--- docs/build.rb
+++ docs/build.rb
@@ -1,6 +1,13 @@
+require 'yaml'
+
 $SPB_ENV = ENV.fetch('SPB_ENV', 'local')
 
 $_.gsub!('@@SPB_ENV@@', $SPB_ENV)
+
+config = YAML.load_file("../config/#{$SPB_ENV}/config.yaml")
+config.each do |key,value|
+  $_.gsub!("@@#{key}@@", value.to_s)
+end
 
 $_.gsub!(/@@config\(([^\)]*)\)@@/) do |f|
   lines = File.read("../config/#{$SPB_ENV}/#{$1}").lines
