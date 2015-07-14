da1e41c79041e2b318c4f4779e2d4939427f1dd9
Gabriela Navarro <navarro1703@gmail.com>
2015-07-14 12:58:27 +0000

    Fix a JS that was breaking all the rest of the JS. It was trying to search a class in all pages instead of just the folder page.
    Signed-off-by: Brenddon Gontijo <brenddongontijo@msn.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Omar Junior <omarroinuj@gmail.com>

--- theme.js
+++ theme.js
@@ -137,11 +137,14 @@ $('#link-buscar').click(function(e) {
   /* Finds all uploaded files from manuals page and sets its names on the right format */
   function set_uploaded_files_names() {
     var article = document.getElementById('article');
-    var folderList = article.getElementsByClassName('folder-content')[0];
-    var folderItens = folderList.getElementsByClassName('item-description');
-
-    for(var i = 0; i < folderItens.length; i++) {
-      split_file_extension(folderItens[i].getElementsByTagName('a')[0]);
+    if(article != null){
+      var folderList = article.getElementsByClassName('folder-content')[0];
+      if(folderList != null){
+        var folderItens = folderList.getElementsByClassName('item-description');
+        for(var i = 0; i < folderItens.length; i++) {
+          split_file_extension(folderItens[i].getElementsByTagName('a')[0]);
+        }
+      }
     }
   }
 
