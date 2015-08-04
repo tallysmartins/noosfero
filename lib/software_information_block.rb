01b77ecb622ed54a508afdd0b7573ce67cb4d9fd
pedrodelyra <pedrodelyra@gmail.com>
2015-08-04 17:56:52 -0300

    Add rating's block to software main page
    Signed-off-by: Fábio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Pedro de Lyra <pedrodelyra@gmail.com>

--- lib/software_information_block.rb
+++ lib/software_information_block.rb
@@ -15,15 +15,24 @@ class SoftwareInformationBlock < Block
   def content(args={})
     block = self
     s = show_name
+    average_rating = CommunityRating.average_rating block.owner.id
+
     lambda do |object|
       render(
         :file => 'blocks/software_information',
-        :locals => { :block => block, :show_name => s }
+        :locals => { :block => block, :show_name => s, :average_rating => average_rating}
       )
     end
   end
 
   def cacheable?
     false
+  end
+
+  private
+
+  def owner_has_ratings?
+    ratings = CommunityRating.where(community_id: block.owner.id)
+    !ratings.empty?
   end
 end
