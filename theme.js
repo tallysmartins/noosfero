6f0f886cd043fc44e8181a7bcee75b94b4dbbd58
pedrodelyra <pedrodelyra@gmail.com>
2015-08-12 17:15:58 -0300

    Adding spb style to software average rating on main page

--- theme.js
+++ theme.js
@@ -190,6 +190,10 @@ $('#link-buscar').click(function(e) {
     $('.star-rate-form .star-comment-container .comments-software-extra-fields #input_institution_comments label').html('Nome do órgão ou empresa');
     $('.star-rate-form .star-comment-container .comments-software-extra-fields .comments-software-people-benefited label').html('Número de beneficiados');
     $('.star-rate-form .star-comment-container .comments-software-extra-fields .comments-software-saved-values label').html('Recursos economizados');
+    $('.community-average-rating-container .star-rate-text').html('Avaliação dos usuários:');
+    $('.community-average-rating-container .rate-this-community a').html('Avalie o software');
+
+
   }
 
   function add_top_tooltips(){
