f90d447a1ce943b1716139ff310097d8681a32a1
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-08-12 15:54:05 -0300

    Created model to community associations
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- colab_spb/models.py
+++ colab_spb/models.py
@@ -1,3 +1,23 @@
-# from django.db import models
+from colab.super_archives import models as mailman
+from django.conf import settings
+from django.db import models
+import importlib
+from sys import modules
 
 # Create your models here.
+# Import plugins models
+for app in settings.COLAB_APPS:
+    if app != 'colab_spb':
+        plugin_name = app.split('.')[-1]
+        # Create alias to plugins
+        exec "%s = importlib.import_module('%s.models')" % (plugin_name, app)
+
+
+class CommunityAssociations(models.Model):
+    community = models.ForeignKey(noosfero.NoosferoCommunity) \
+        if 'noosfero' in modules else None
+
+    repository = models.ForeignKey(gitlab.GitlabProject) \
+        if 'gitlab' in modules else None
+
+    mail_list = models.ForeignKey(mailman.MailingList, null=True)
