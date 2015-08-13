e6a4c887992e96daf5babb6a1127a439001d19b2
Antonio Terceiro <terceiro@softwarelivre.org>
2015-08-13 17:11:17 -0300

    enable/start nginx on munin master

--- cookbooks/munin/recipes/default.rb
+++ cookbooks/munin/recipes/default.rb
@@ -4,6 +4,7 @@ template '/etc/munin/conf.d/hosts.conf'
 
 package 'nginx'
 service 'nginx' do
+  action [:enable, :start]
   supports :reload => true
 end
 cookbook_file '/etc/nginx/default.d/munin.conf' do
