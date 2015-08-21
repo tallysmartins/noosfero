dc1b1c8899283923496d7a38e053bb687fe265ed
Sergio Oliveira <sergio@tracy.com.br>
2015-08-21 10:55:59 -0300

    Fixed indentation
    Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>

--- cookbooks/rsyslog/recipes/node.rb
+++ cookbooks/rsyslog/recipes/node.rb
@@ -1,16 +1,16 @@
 # This cookbook installs a cliente rsyslog
 
 package 'rsyslog' do
-	action [:install, :upgrade]
+  action [:install, :upgrade]
 end
 
 template '/etc/rsyslog.d/node.conf' do
-	source "node/node.conf.erb"
-	owner 'root'
-	group 'root'
-	mode 0644
+  source "node/node.conf.erb"
+  owner 'root'
+  group 'root'
+  mode 0644
 end
 
 service 'rsyslog' do
-	action [:enable, :restart]
+  action [:enable, :restart]
 end
--- cookbooks/rsyslog/recipes/server.rb
+++ cookbooks/rsyslog/recipes/server.rb
@@ -1,16 +1,16 @@
 # This cookbook installs a server rsyslog
 
 package 'rsyslog' do
-	action [:install, :upgrade]
+  action [:install, :upgrade]
 end
 
 template '/etc/rsyslog.d/server.conf' do
-	source 'server/server.conf.erb'
-	owner 'root'
-	group 'root'
-	mode 0644
+  source 'server/server.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
 end
 
 service 'rsyslog' do
-	action [:enable, :restart]
+  action [:enable, :restart]
 end
