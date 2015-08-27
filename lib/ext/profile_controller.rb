878fa51be6769ac2e2c2b7491df3775c6b72b84b
Tallys Martins <tallysmartins@yahoo.com.br>
2015-08-27 17:34:26 -0300

    Add page Members header
    Signed-off-by: Tallys Martins <tallysmartins@yahoo.com.br>

--- lib/ext/profile_controller.rb
+++ lib/ext/profile_controller.rb
@@ -20,4 +20,21 @@ class ProfileController
       @communities = type.paginate(:per_page => per_page, :page => params[:npage], :total_entries => type.count)
     end
   end
+
+  def members
+    if is_cache_expired?(profile.members_cache_key(params))
+      all_members = if params[:sort] and params[:sort] == "desc"
+        profile.members.order("name desc")
+      else
+        profile.members.order("name asc")
+      end
+      @profile_admins = profile.admins
+      @profile_members = all_members - @profile_admins
+      @profile_members = @profile_members.paginate(:per_page => members_per_page, :page => params[:npage], :total_entries => @profile_members.size)
+      @profile_admins = @profile_admins.paginate(:per_page => members_per_page, :page => params[:npage], :total_entries => @profile_admins.size)
+      @total_members = all_members.size
+      @profile_members_url = url_for(:controller => "profile", :action => "members")
+    end
+  end
+
 end
