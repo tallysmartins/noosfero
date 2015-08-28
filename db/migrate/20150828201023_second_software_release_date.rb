12fd7b7558fa31f16783f8469fe1e227ea88e0c0
Arthur Del Esposte <arthurmde@gmail.com>
2015-08-28 21:21:17 +0000

    Add a second migration to change softwares dates

---
+++ db/migrate/20150828201023_second_software_release_date.rb
@@ -0,0 +1,40 @@
+class SecondSoftwareReleaseDate < ActiveRecord::Migration
+  def up
+    softwares = SoftwareInfo.all
+    softwares.each do |software|
+      if software.community
+        name = software.community.name.strip
+        software.community.name = name
+        software.community.save
+      else
+        software.destroy
+      end
+    end
+
+    file = File.new("plugins/spb_migrations/files/date-communities.txt", "r")
+    while (line = file.gets)
+      result = line.split('|')
+      software_name = result[2].gsub("/n", "")
+      software = Community.where("name ILIKE ?", software_name.strip)
+
+      if software && software.count == 1
+        software = software.first
+        software.created_at = Time.zone.parse(result[1])
+        if software.save
+          print "."
+        else
+          print "F"
+        end
+      else
+        print "F"
+        puts software_name
+      end
+    end
+    file.close
+    puts ""
+  end
+
+  def down
+    say "This can't be reverted"
+  end
+end
