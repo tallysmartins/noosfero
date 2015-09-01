bf01b5dafd8f791c5d6068b2ff2130ec7bd3fbce
Paulo Tada <paulohtfs@gmail.com>
2015-09-01 08:41:32 -0300

    Changing recipes node and server to default recipe

--- cookbooks/rsyslog/recipes/default.rb
+++ cookbooks/rsyslog/recipes/default.rb
@@ -1,11 +1,10 @@
-# This cookbook installs a server rsyslog
+# This cookbook installs a cliente rsyslog
 
 package 'rsyslog' do
   action [:install, :upgrade]
 end
 
-template '/etc/rsyslog.d/server.conf' do
-  source 'server/server.conf.erb'
+template "/etc/rsyslog.d/spb_log.conf" do
   owner 'root'
   group 'root'
   mode 0644
