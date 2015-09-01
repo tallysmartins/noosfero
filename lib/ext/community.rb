9a9baa5313cfe365e6c6ec326861a6cc0598dc20
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-09-02 01:11:58 +0200

    Fix for filter_communities on search communities with another plugin

--- lib/ext/community.rb
+++ lib/ext/community.rb
@@ -3,8 +3,20 @@ require_dependency 'community'
 class Community
   has_one :institution, :dependent=>:destroy
 
-
   def institution?
     return !institution.nil?
+  end
+
+  def remove_of_community_search_institution?
+    return institution?
+  end
+
+  def self.get_valid_communities_string
+    remove_of_communities_methods = Community.instance_methods.select{|m| m =~ /remove_of_community_search/}
+    valid_communities_string = ""
+    remove_of_communities_methods.each do |method|
+      valid_communities_string += "!community.send('#{method}') && "
+    end
+    valid_communities_string[0..-5]
   end
 end
