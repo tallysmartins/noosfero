63bae030c8f879e6a1201e872ddf3c297dcd0a67
Arthur Del Esposte <arthurmde@gmail.com>
2015-09-05 14:19:18 -0300

    Fix migrations to consider software template

--- db/migrate/20150904181508_add_communities_ratings_block_to_all_softwares_communities.rb
+++ db/migrate/20150904181508_add_communities_ratings_block_to_all_softwares_communities.rb
@@ -1,12 +1,34 @@
 class AddCommunitiesRatingsBlockToAllSoftwaresCommunities < ActiveRecord::Migration
   def up
+    software_template = Community["software"]
+    if software_template
+      software_area_one = software_template.boxes.find_by_position 1
+
+      template_ratings_block = CommunitiesRatingsBlock.new :mirror => true, :move_modes => "none", :edit_modes => "none"
+      template_ratings_block.settings[:fixed] = true
+      template_ratings_block.display = "home_page_only"
+      template_ratings_block.save!
+      print "."
+
+      software_area_one.blocks << template_ratings_block
+      software_area_one.save!
+      print "."
+
+      # Puts the ratings block as the last one on area one
+      last_block_position = software_area_one.blocks.order(:position).last.position
+      template_ratings_block.position = last_block_position + 1
+      template_ratings_block.save!
+      print "."
+    end
+
     Community.joins(:software_info).each do |software_community|
       software_area_one = software_community.boxes.find_by_position 1
       print "."
 
-      ratings_block = CommunitiesRatingsBlock.new :mirror => true, :move_modes => "none", :edit_modes => "none"
+      ratings_block = CommunitiesRatingsBlock.new :move_modes => "none", :edit_modes => "none"
       ratings_block.settings[:fixed] = true
       ratings_block.display = "home_page_only"
+      ratings_block.mirror_block_id = template_ratings_block.id
       ratings_block.save!
       print "."
 
@@ -25,6 +47,6 @@ class AddCommunitiesRatingsBlockToAllSoftwaresCommunities < ActiveRecord::Migrat
   end
 
   def down
-    CommunitiesRatingsBlock.destroy_all
+    say "This can't be reverted"
   end
 end
