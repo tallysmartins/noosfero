c070230f21d0d81c2f7df62ada85afbeaa1d1634
Fabio Teixeira <fabio1079@gmail.com>
2015-09-09 16:25:35 -0300

    Add wiki block to all softwares communities migration
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Simiao Carvalho <simiaosimis@gmail.com>

---
+++ db/migrate/20150909191415_add_wiki_block_to_all_softwares_communities.rb
@@ -0,0 +1,54 @@
+class AddWikiBlockToAllSoftwaresCommunities < ActiveRecord::Migration
+  def up
+    software_template = Community["software"]
+
+    if software_template
+      software_area_two = software_template.boxes.find_by_position 2
+
+      wiki_block_template = WikiBlock.new :mirror => true, :move_modes => "none", :edit_modes => "none"
+      wiki_block_template.settings[:fixed] = true
+      wiki_block_template.save!
+      print "."
+
+      software_area_two.blocks << wiki_block_template
+      software_area_two.save!
+      print "."
+
+      # Puts the ratings block as the last one on area one
+      repository_block = software_area_two.blocks.find_by_type("RepositoryBlock")
+      if !repository_block.nil?
+        wiki_block_template.position = repository_block.position + 1
+        wiki_block_template.save!
+        print "."
+      end
+    end
+
+    Community.joins(:software_info).each do |software_community|
+      software_area_two = software_community.boxes.find_by_position 2
+      print "."
+
+      wiki_block = WikiBlock.new :move_modes => "none", :edit_modes => "none"
+      wiki_block.settings[:fixed] = true
+      wiki_block.mirror_block_id = wiki_block_template.id
+      wiki_block.save!
+      print "."
+
+      software_area_two.blocks << wiki_block
+      software_area_two.save!
+      print "."
+
+      repository_block = software_area_two.blocks.find_by_type("RepositoryBlock")
+      if !repository_block.nil?
+        wiki_block.position = repository_block.position + 1
+        wiki_block.save!
+        print "."
+      end
+    end
+
+    puts ""
+  end
+
+  def down
+    say "This can't be reverted"
+  end
+end
