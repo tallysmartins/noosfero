80ff4efa853ca112423caba0a7978d3d078c9bb6
Sergio Oliveira <sergio@tracy.com.br>
2015-09-20 19:33:04 -0300

    Added unicode method to help on associations

--- src/colab_spb/models.py
+++ src/colab_spb/models.py
@@ -8,3 +8,8 @@ class CommunityAssociations(models.Model):
     community = models.ForeignKey(noosfero.NoosferoCommunity, null=True)
     group = models.ForeignKey(gitlab.GitlabGroup, null=True)
     mail_list = models.ForeignKey(mailman.MailingList, null=True)
+
+    def __unicode__(self):
+        return u'Social: {} - Dev: {} - List: {}'.format(self.community.name,
+                                                         self.group.name,
+                                                         self.mail_list.name)
