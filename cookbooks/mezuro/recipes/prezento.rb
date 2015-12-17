1bbbc554aa570d13584bb5e975677cfea4bc2aa2
Paulo Tada <paulohtfs@gmail.com>
2015-12-17 15:34:50 -0200

    Adding the kalibro configurations for prezento
    Signed-off-by: Diego Araújo <diegoamc90@gmail.com>

--- cookbooks/mezuro/recipes/prezento.rb
+++ cookbooks/mezuro/recipes/prezento.rb
@@ -5,18 +5,18 @@ execute 'download:mezuro' do
   user 'root'
 end
 
+package 'prezento'
+
+service 'prezento.target' do
+  action [:enable, :start]
+end
+
 template '/etc/mezuro/prezento/database.yml' do
   source 'prezento/database.yml.erb'
   owner 'prezento'
   group 'prezento'
   mode '0600'
   notifies :restart, 'service[prezento.target]'
-end
-
-package 'prezento'
-
-service 'prezento.target' do
-  action [:enable, :start]
 end
 
 PREZENTO_DIR='/usr/share/mezuro/prezento'
@@ -34,10 +34,22 @@ execute 'prezento:migrate' do
   notifies :restart, 'service[prezento.target]'
 end
 
+template PREZENTO_DIR + '/config/kalibro.yml' do
+  source 'prezento/kalibro.yml.erb'
+  owner 'prezento'
+  group 'prezento'
+  mode '0644'
+  notifies :restart, 'service[prezento.target]'
+end
+
 #############################################################################
 # The next lines install the mezuro plugin for colab intergration           #
 # This shold be removed when the plugin were install thought pkg colab-deps #
 #############################################################################
+
+service 'colab' do
+  action [:enable, :start]
+end
 
 git '/home/vagrant/colab-mezuro-plugin' do
   repository 'https://github.com/colab/colab-mezuro-plugin.git'
@@ -48,4 +60,6 @@ execute 'pip:install:plugin' do
   command '/usr/lib/colab/bin/pip install .'
   cwd '/home/vagrant/colab-mezuro-plugin'
   user 'root'
+  notifies :restart, 'service[colab]'
 end
+
