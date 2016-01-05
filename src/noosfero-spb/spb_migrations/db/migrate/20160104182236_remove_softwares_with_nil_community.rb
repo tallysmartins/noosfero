4febc283c1e9aafdcc755454c1acbad38475daf6
Thiago Ribeiro <thiagitosouza@gmail.com>
2016-01-05 11:03:34 -0200

    Remove softwre with nil community.

---
+++ src/noosfero-spb/spb_migrations/db/migrate/20160104182236_remove_softwares_with_nil_community.rb
@@ -0,0 +1,9 @@
+class RemoveSoftwareWithoutCommunity < ActiveRecord::Migration
+  def up
+    execute('DELETE FROM software_infos where community_id IS NULL;')
+  end
+
+  def down
+    say"This migration can't be reverted"
+  end
+end
