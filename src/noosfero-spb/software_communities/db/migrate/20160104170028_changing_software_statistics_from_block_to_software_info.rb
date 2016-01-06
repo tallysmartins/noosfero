c100e4e86e5d4b457ad3f5aecd463fca882ae440
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2016-01-06 13:25:57 -0300

    Fix: migration skips softwares without community

--- src/noosfero-spb/software_communities/db/migrate/20160104170028_changing_software_statistics_from_block_to_software_info.rb
+++ src/noosfero-spb/software_communities/db/migrate/20160104170028_changing_software_statistics_from_block_to_software_info.rb
@@ -4,14 +4,16 @@ class ChangingSoftwareStatisticsFromBlockToSoftwareInfo < ActiveRecord::Migratio
       benefited_people = 0
       saved_resources = 0
 
-      select_all("SELECT * FROM tasks WHERE status=3 AND type='CreateOrganizationRatingComment' AND target_id=#{software['community_id']}").each do |task|
-        settings = YAML.load(task['data'])
-        organization_rating = select_one("SELECT * FROM organization_ratings WHERE id=#{settings[:organization_rating_id]}")
+      if software['community_id']
+        select_all("SELECT * FROM tasks WHERE status=3 AND type='CreateOrganizationRatingComment' AND target_id=#{software['community_id']}").each do |task|
+          settings = YAML.load(task['data'])
+          organization_rating = select_one("SELECT * FROM organization_ratings WHERE id=#{settings[:organization_rating_id]}")
 
-        benefited_people += organization_rating["people_benefited"].to_i
-        saved_resources += organization_rating["saved_value"].to_f
+          benefited_people += organization_rating["people_benefited"].to_i
+          saved_resources += organization_rating["saved_value"].to_f
+        end
+        execute("UPDATE software_infos SET benefited_people=#{benefited_people}, saved_resources=#{saved_resources} WHERE id=#{software['id']}")
       end
-      execute("UPDATE software_infos SET benefited_people=#{benefited_people}, saved_resources=#{saved_resources} WHERE id=#{software['id']}")
     end
 
   end
