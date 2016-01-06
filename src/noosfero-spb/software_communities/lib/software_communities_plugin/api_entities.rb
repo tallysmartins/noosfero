8c0dc128b106aa466429bbefc672d618f7578d17
Marcos Ronaldo <marcos.rpj2@gmail.com>
2016-01-06 16:11:38 -0200

    fix showing license_info in software API
    Signed-off-by: Marcos Ronaldo <marcos.rpj2@gmail.com>
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- src/noosfero-spb/software_communities/lib/software_communities_plugin/api_entities.rb
+++ src/noosfero-spb/software_communities/lib/software_communities_plugin/api_entities.rb
@@ -1,10 +1,26 @@
 module Entities
+
+  class LicenseInfo < Noosfero::API::Entity
+    expose :version
+    expose :link
+  end
+
   class SoftwareInfo < Noosfero::API::Entity
     root 'software_infos', 'software_info'
-    expose :id, :finality, :repository_link, :public_software, :acronym, :objectives,
-                :features,:license_info, :software_languages, :software_databases, :operating_system_names
+    expose :id
+    expose :finality
+    expose :repository_link
+    expose :public_software
+    expose :acronym
+    expose :objectives
+    expose :features
+    expose :license_info, :using => LicenseInfo
+    expose :software_languages
+    expose :software_databases
+    expose :operating_system_names
     expose :community_id do |software_info,options|
       software_info.community.id
     end
   end
+
 end
