adc7e9beb80645bb76bd1fe78ff5abf96281255e
Tallys Martins <tallysmartins@yahoo.com.br>
2016-01-12 02:17:30 -0200

    Multi env on SoftwareInfo scope search_by_query
    Signed-off-by: Tallys Martins <tallysmartins@yahoo.com.br>

--- src/noosfero-spb/software_communities/lib/software_info.rb
+++ src/noosfero-spb/software_communities/lib/software_info.rb
@@ -13,15 +13,15 @@ class SoftwareInfo < ActiveRecord::Base
     DatabaseDescription
   ]
 
-  scope :search_by_query, lambda { |query = ""|
+  scope :search_by_query, lambda { |query = "", env = Environment.default|
     filtered_query = query.gsub(/[\|\(\)\\\/\s\[\]'"*%&!:]/,' ').split.map{|w| w += ":*"}.join('|')
     search_fields = SoftwareInfo.pg_search_plugin_fields
 
     if query.empty?
-      SoftwareInfo.joins(:community).where("profiles.visible = ?", true)
+      SoftwareInfo.joins(:community).where("profiles.visible = ? AND environment_id = ? ", true, env.id)
     else
       searchable_software_objects = SoftwareInfo.transform_list_in_methods_list(SEARCHABLE_SOFTWARE_CLASSES)
-      includes(searchable_software_objects).where("to_tsvector('simple', #{search_fields}) @@ to_tsquery('#{filtered_query}')").where("profiles.visible = ?", true)
+      includes(searchable_software_objects).where("to_tsvector('simple', #{search_fields}) @@ to_tsquery('#{filtered_query}')").where("profiles.visible = ? AND environment_id = ?", true, env.id)
     end
   }
 
