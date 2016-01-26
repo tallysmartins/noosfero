8d610ddd3702c06b9762f7830c183a009ecb19c5
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-01-26 17:30:45 +0000

    Fix protection of create_institution_admin
    Signed-off-by: Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>

--- src/noosfero-spb/gov_user/controllers/gov_user_plugin_controller.rb
+++ src/noosfero-spb/gov_user/controllers/gov_user_plugin_controller.rb
@@ -5,6 +5,8 @@ class GovUserPluginController < ApplicationController
     :governmental_sphere, :governmental_power, :juridical_nature, :sisp
   ]
 
+  protect "create_institution_admin", :environment
+
   def hide_registration_incomplete_percentage
     response = false
 
