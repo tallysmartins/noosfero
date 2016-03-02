0c0dedf58404aef7023abee3050398787f487bfa
Carlos Coelho <carlos@pencillabs.com>
2016-03-02 11:56:39 -0300

    Create relation only if it doesnt exist
    Signed-off-by: Carlos Coelho <carlos@pencillabs.com>

--- src/colab-spb-plugin/src/colab_spb/migrations/0002_auto_20160229_1735.py
+++ src/colab-spb-plugin/src/colab_spb/migrations/0002_auto_20160229_1735.py
@@ -3,28 +3,28 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+
 class Migration(migrations.Migration):
 
     def match_communities_mailinglists(apps, schema_editor):
-        NoosferoCommunity = apps.get_model("colab_noosfero",
-                                           "NoosferoCommunity")
+        NoosferoCommunity = apps.get_model(
+            "colab_noosfero", "NoosferoCommunity"
+        )
         MailingList = apps.get_model("super_archives", "MailingList")
-        CommunityAssociations = apps.get_model("colab_spb",
-                                               "CommunityAssociations")
+        Associations = apps.get_model("colab_spb", "CommunityAssociations")
 
         for community in NoosferoCommunity.objects.all():
             try:
-                maillist = MailingList.objects.get(name__iexact=community.identifier)
+                maillist = MailingList.objects.get(
+                    name__iexact=community.identifier
+                )
 
-                community_association = CommunityAssociations()
-
-                community_association.community = community
-                community_association.mail_list = maillist
-
-                community_association.save()
+                association = Associations.objects.get_or_create(
+                    mail_list=maillist,
+                    community=community
+                )
             except MailingList.DoesNotExist:
                 continue
-
 
     dependencies = [
         ('colab_spb', '0001_initial'),
--- src/colab-spb-plugin/src/colab_spb/migrations/0003_auto_20160229_1804.py
+++ src/colab-spb-plugin/src/colab_spb/migrations/0003_auto_20160229_1804.py
@@ -8,20 +8,20 @@ class Migration(migrations.Migration):
 
     def associate_communities_groups(apps, schema_editor):
         GitlabGroup = models.get_model("colab_gitlab", "GitlabGroup")
-        NoosferoCommunity = models.get_model("colab_noosfero",
-                                             "NoosferoCommunity")
-        CommunityAssociations = models.get_model("colab_spb",
-                                                 "CommunityAssociations")
+        CommunityAssociations = models.get_model(
+            "colab_spb", "CommunityAssociations"
+        )
 
         for community_association in CommunityAssociations.objects.all():
             try:
                 community = community_association.community
-                group = GitlabGroup.objects.get(name__iexact=community.identifier)
+                group = GitlabGroup.objects.get(
+                    name__iexact=community.identifier
+                )
                 community_association.group = group
                 community_association.save()
             except GitlabGroup.DoesNotExist:
                 continue
-
 
     dependencies = [
         ('colab_spb', '0002_auto_20160229_1735'),
