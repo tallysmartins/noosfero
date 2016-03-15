644cf0d1760256704b664088c8b4a33f179d3c16
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-03-15 12:27:31 +0000

    Fix namespace on spb_migrations

--- src/noosfero-spb/spb_migrations/db/migrate/20160104182236_remove_softwares_with_nil_community.rb
+++ src/noosfero-spb/spb_migrations/db/migrate/20160104182236_remove_softwares_with_nil_community.rb
@@ -1,6 +1,6 @@
 class RemoveSoftwaresWithNilCommunity < ActiveRecord::Migration
   def up
-    execute('DELETE FROM software_infos where community_id IS NULL;')
+    execute('DELETE FROM software_communities_plugin_software_infos where community_id IS NULL;')
   end
 
   def down
