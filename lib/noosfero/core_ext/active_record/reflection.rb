6b693f8739d5af251ea70cd147c6b572669b189f
Marcos Ronaldo <marcos.rpj2@gmail.com>
2016-05-16 10:02:30 -0300

    Revert "Fixes ActiveRecord relations for descendants"
    This reverts commit 6607dc221143955c2b42f967c3e5d4de6948d1e9.

--- lib/noosfero/core_ext/active_record/reflection.rb
+++ lib/noosfero/core_ext/active_record/reflection.rb
@@ -1,28 +1,20 @@
+
 # on STI classes tike Article and Profile, plugins' extensions
 # on associations should be reflected on descendants
 module ActiveRecord
-  module Associations
-    def association(name) #:nodoc:
-      association = association_instance_get(name)
+  module Reflection
 
-      if association.nil?
-        reflection = self.class._reflect_on_association(name)
+    class << self
 
-        # Check inheritance chain for possible upstream association
-        klass = self.class.superclass
-        while reflection.nil? && klass.respond_to?(:_reflect_on_association)
-          reflection = klass._reflect_on_association(name)
-          klass = klass.superclass
-        end
-
-        unless reflection
-          raise AssociationNotFoundError.new(self, name)
-        end
-        association = reflection.association_class.new(self, reflection)
-        association_instance_set(name, association)
+      def add_reflection_with_descendants(ar, name, reflection)
+        self.add_reflection_without_descendants ar, name, reflection
+        ar.descendants.each do |k|
+          k._reflections.merge!(name.to_s => reflection)
+        end if ar.base_class == ar
       end
 
-      association
+      alias_method_chain :add_reflection, :descendants
+
     end
   end
 end
