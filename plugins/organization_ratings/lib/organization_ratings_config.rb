ad0090f5a6f0427d37cad0bc783d8464679bf377
Marcos Ronaldo <marcos.rpj2@gmail.com>
2016-05-23 16:52:48 -0300

    organization_ratings: add intial_page option
    (cherry picked from commit b08287d1c8fe632d9fa4e11477594bd02345f294)

--- plugins/organization_ratings/lib/organization_ratings_config.rb
+++ plugins/organization_ratings/lib/organization_ratings_config.rb
@@ -4,6 +4,7 @@ class OrganizationRatingsConfig < ActiveRecord::Base
 
   attr_accessible :cooldown, :default_rating, :order, :per_page
   attr_accessible :vote_once, :are_moderated, :environment_id
+  attr_accessible :ratings_on_initial_page
 
   ORDER_OPTIONS = {recent: _('More Recent'), best: _('Best Ratings')}
 
